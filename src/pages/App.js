7ba5f18d832ad84c28f94dd5e2c089bd9f2ed9b3
Angellotz <angellosilvam@hotmail.com>
2024-11-06 12:40:12 -0300

    filter iplementado

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import gitLogo from '../assets/github.png'
 import Input from '../components/Input';
@@ -13,39 +12,38 @@ function App() {
   const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
 
-
   const handleSearchRepo = async () => {
+    try {
+      const { data } = await api.get(`repos/${currentRepo}`);
 
-    const {data} = await api.get(`repos/${currentRepo}`)
+      if (data.id) {
+        // Verifica se o repositório já existe na lista
+        const isExist = repos.find(repo => repo.id === data.id);
 
-    if(data.id){
-
-      const isExist = repos.find(repo => repo.id === data.id);
-
-      if(!isExist){
-        setRepos(prev => [...prev, data]);
-        setCurrentRepo('')
-        return
+        if (!isExist) {
+          setRepos(prev => [...prev, data]);
+          setCurrentRepo('');
+          return;
+        }
       }
-
+      alert('Repositório não encontrado');
+    } catch (error) {
+      alert('Erro ao buscar repositório');
     }
-    alert('Repositório não encontrado')
-
-  }
+  };
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
-  }
-
+    // Filtra o repositório para remover o item com o id especificado
+    const updatedRepos = repos.filter(repo => repo.id !== id);
+    setRepos(updatedRepos);
+  };
 
   return (
     <Container>
-      <img src={gitLogo} width={72} height={72} alt="github logo"/>
+      <img src={gitLogo} width={72} height={72} alt="github logo" />
       <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
-      <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+      <Button onClick={handleSearchRepo} />
+      {repos.map(repo => <ItemRepo key={repo.id} handleRemoveRepo={handleRemoveRepo} repo={repo} />)}
     </Container>
   );
 }
